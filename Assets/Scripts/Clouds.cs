b75e418917126b48e3bd6c8db4b6b6a700db4637
Mario Camara Ramil <mario.camara@enti.cat>
2018-04-18 22:03:30 +0200

    Modificaciones UI
    -Se han añadido Pantallas iniciales con logo de akona y grinder
    -Se ha modificado la Pantalla de Menú principal
    ToDo: Hay que hacer que se enlacen, y acabar el menú principal.

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clouds : MonoBehaviour {
+    public GameObject cloud;
+    float timer;
+    float absoluteTime;
+    // Use this for initialization
+    void Start () {
+        absoluteTime = 4.0f;
+        timer = absoluteTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= 1 * Time.deltaTime;
+        if (timer <= 0) {
+            GameObject newCloud = Instantiate(cloud, new Vector3(0, 8, 0), Quaternion.identity) as GameObject;
+            newCloud.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            RectTransform newCloudRT = newCloud.GetComponent<RectTransform>();
+            newCloudRT.SetAsFirstSibling();
+            timer = absoluteTime;
+        }
+    }
+}
---
+++ Assets/Scripts/CloudsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsController : MonoBehaviour {
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public float speed;
+    private float startTime;
+    private float journeyLength;
+
+    // Use this for initialization
+    void Start () {
+        int ypos = Random.Range(-3, 5);
+        startTime = Time.time;
+        pos1 = new Vector3(-5, ypos, 0);
+        pos2 = new Vector3(5, ypos, 0);
+        journeyLength = Vector3.Distance(pos1, pos2);
+        transform.position = pos1;
+        speed = Random.Range(0.5F, 1.5F);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(pos1, pos2, fracJourney);
+
+        if (transform.position == pos2) {
+            DestroyGameObject();
+        }
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+}
