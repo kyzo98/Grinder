ed8a9b4bfb9e7d2840233b62629ca1e5efc44159
Paul Moya <paul.moya@enti.cat>
2018-04-20 17:12:54 +0200

    UI Fade

--- Assets/Scripts/JumpScene2.cs
+++ Assets/Scripts/JumpScene2.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
 
 public class JumpScene2 : MonoBehaviour {
 
     public string SceneToLoad = "GrinderTitleScene";
-    public float DelayTime = 3.0f;
-    public void Start()
+
+    public Image akona;
+   
+     IEnumerator Start()
     {
-        StartCoroutine("Wait");
-    }
-    private IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(DelayTime);
+       akona.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(3.0f); 
 
         SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Single);
 
+    }
+
+    void FadeIn()
+    {
+        akona.CrossFadeAlpha(1.0f, 1.0f, false); 
     }
 
 } 
--- Assets/Scripts/LoadMenu.cs
+++ Assets/Scripts/LoadMenu.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
 
 public class LoadMenu : MonoBehaviour {
 
     public string SceneToLoad = "Menu";
-    public float DelayTime = 3.0f;
-    public void Start()
+
+    public Image grinder;
+
+    IEnumerator Start()
     {
-        StartCoroutine("Wait");
-    }
-    private IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(DelayTime);
+        grinder.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(3.0f);
 
         SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Single);
 
     }
+
+    void FadeIn()
+    {
+        grinder.CrossFadeAlpha(1.0f, 1.0f, false);
+    }
+
 }
