f97fd1e3ca907cc2fffa273637e0a22c40de6839
Unknown <juan.lojo@enti.cat>
2018-04-23 19:57:33 +0100

    Added some text as a tutorial.

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    public Text tutorialText;
+
+	// Use this for initialization
+	void Start () {
+        tutorialText.text = "Tu objetivo es alcanzar la mayor profunidad. Pulsa las flechas para moverte.";
+        Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            Time.timeScale = 0;
+            Destroy(gameObject);
+        }
+    }
+}
