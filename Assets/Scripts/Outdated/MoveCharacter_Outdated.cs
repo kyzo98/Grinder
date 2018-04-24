8fc253aedf60a57dae5367fc266d324e4b808e84
Mario Camara Ramil <mario.camara@enti.cat>
2018-04-24 22:10:53 +0200

    Arreglao

--- Assets/Scripts/Outdated/MoveCharacter_Outdated.cs
+++ Assets/Scripts/Outdated/MoveCharacter_Outdated.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoveCharacter : MonoBehaviour {
+public class MoveCharacter_Outdated : MonoBehaviour {
     public enum Move { Down, Right, Left };
     public GameObject box;
     public Image scrollBar;
